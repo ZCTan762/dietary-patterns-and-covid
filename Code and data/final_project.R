98d2ac88ba2d63dad4f799822c62cab2d76d22c9
Zack Tan <zt00063@surrey.ac.uk>
2021-12-11 17:00:53 -0500

    Update. Made the following changes: - Restructured code. To run covmatrix before PCA due to large number of predictors. - Added code for PCA analysis - but may be better to use random forest for importance.

--- Code and data/final_project.R
+++ Code and data/final_project.R
@@ -44,15 +44,22 @@ attach(combined)
 names(combined)
 ncol(combined)
 
-# Drop rows with NA entries in target - Ananya
+# Keep only Deaths as a target predictor
+drop_list <- c('Active', 'Recovered', 'Confirmed')
+combined <- combined[ , !names(combined) %in% drop_list]
+
+# check for nulls 
+nulls <- colSums(is.na(combined))
+na_count <- data.frame( nulls)
+na_count
+
+# Drop rows with NAs in target var
+library(tidyr)
+# TODO: Drop 1 NA observation in Obesity TEMP
+combined <- combined %>% drop_na('Deaths') %>% drop_na('Obesity')
 
 
-# Select only numeric predictors
-# Removes 'undernourished' column - contains greater than and less than signs
-# combined_numeric <- combined %>%
-#   select_if(is.numeric)
-
-# Treat Undernourished - Replace all '<2.5' with 1.5. And divide into 3 bins by value
+# Treat Undernourished - Replace all '<2.5' with 1.5 and divide into 3 bins by value
 combined$Undernourished=ifelse(combined$Undernourished=="<2.5",1.5, combined$Undernourished)
 combined$Undernourished <- cut(combined$Undernourished, breaks = 3, labels = c("Low", "Mid", "High"))
 # Move NA  
@@ -60,14 +67,48 @@ combined$Undernourished <- factor(ifelse(is.na(combined$Undernourished),
                                          "No Data", paste(combined$Undernourished)), 
                                   levels = c(levels(combined$Undernourished), "No Data"))
 
+### Exclude Undernourished column for numerical analyses
+combined_numeric <- combined[,-25]
 
 # Create correlation matrix
 cor_matrix <- cor(combined_numeric, method = c("pearson"))
-round(cor_matrix, 3)
-write.csv(cor_matrix, file = "cormatrix.csv")
+write.csv(round(cor_matrix, 3), file = "cormatrix.csv")
 
 
+##################### WORKS TILL HERE #####################
 
-# dataf[, c(1, 2, 3)]
-# dataf[, c('A', 'B', 'Cost')]
 
+# Separate out the target
+target = combined[,c(26)]
+predictors = combined[,-26]
+
+# Perform PCA -  
+pca=prcomp(pca_data, scale=TRUE)
+summary(pca)
+
+# Scree plot for PCA - Good for report
+plot(pca, type = "l", main = "Scree plot for PCA")
+
+### The first 2 components explain only 32% of variance. Use correlation matrix 
+### to shave down first instead.
+
+# Find variable importance through PCA (only numeric)
+
+# PC1
+loading_Scores_PC1 <- pca$rotation[,1]       # describe how much each attribute contributes to the principal component
+influence_PC1 <- abs(loading_Scores_PC1)     # rank in order of influence, ignore negative/positive influence
+influence_PC1ranked <- names(sort(influence_PC1,decreasing = T))    # Output components of PC in order of in
+
+# PC2
+loading_Scores_PC2 <- pca$rotation[,2]       # describe how much each attribute contributes to the principal component
+influence_PC2 <- abs(loading_Scores_PC2)     # rank in order of influence, ignore negative/positive influence
+influence_PC2ranked <- names(sort(influence_PC2,decreasing = T))    # Output components of PC in order of in
+
+# % variance explained
+pve=(pca$sdev^2)/sum(pca$sdev^2)
+par(mfrow=c(1,2))
+plot(pve, ylim=c(0,1))
+plot(cumsum(pve), ylim=c(0,1))        # Cumulative sum
+par(mfrow=c(1,1))
+
+autoplot(pca, data = pca_data, loadings = TRUE, col="grey", loadings.label = TRUE )
